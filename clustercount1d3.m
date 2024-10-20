f010bacec34da6d48567a049c7c876507a8a0bfb
Fomalhaut <75649873+shenwang3333@users.noreply.github.com>
2024-10-20 20:59:11 +0800

    Add files via upload

---
+++ clustercount1d3.m
@@ -0,0 +1,86 @@
+function [counter] = clustercount1d2(seq)
+
+len = length(seq);
+counter = 0;
+cluster_cnt = 0;
+
+
+for j = 2:len
+
+	if seq(j) == 'E' || seq(j) == 'D'
+
+		if seq(j-1) == 'E' || seq(j-1) == 'D'
+
+			cluster_cnt = cluster_cnt + 1;
+
+			counter = counter + cluster_cnt;
+
+		else 
+
+			counter = counter;
+
+		end
+
+	end
+
+	if j == 2 && (seq(j) ~= 'E' && seq(j) ~= 'D')
+			
+		if seq(j-1) == 'E' || seq(j-1) == 'D' 
+
+			%%cluster_cnt = cluster_cnt - 1;
+			%%cluster_cnt(cluster_cnt < 0) = 0;
+
+			counter = counter;
+
+		else
+			
+			cluster_cnt = 0;
+
+			counter = counter;
+
+		end
+
+	end
+
+	if j > 2 && (seq(j) ~= 'E' && seq(j) ~= 'D') && (seq(j) ~= 'K' && seq(j) == 'R')
+		
+		if seq(j-1) == 'E' || seq(j-1) == 'D' 
+
+			%%cluster_cnt = cluster_cnt - 1;
+			%%cluster_cnt(cluster_cnt < 0) = 0;
+
+			counter = counter;
+
+		elseif (seq(j-1) ~= 'E' && seq(j-1) ~= 'D') && (seq(j-2) == 'E' || seq(j-2) ~= 'D')
+
+			%%cluster_cnt = cluster_cnt - 2;
+			%%cluster_cnt(cluster_cnt < 0) = 0;
+
+			counter = counter;
+
+		else
+
+			cluster_cnt = 0;
+
+			counter = counter;
+
+		end
+
+	end
+
+	if j > 2 && (seq(j) == 'K' || seq(j) == 'R') && (seq(j-1) == 'E' || seq(j-1) == 'D') && (seq(j-2) == 'E' || seq(j-2) == 'D')
+
+		cluster_cnt = 0;
+
+		counter = counter;
+
+	else
+		
+		counter = counter;
+
+	end
+
+end
+
+
+
